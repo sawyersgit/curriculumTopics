308cd7091bac7749e5cad512706101f58246b2df
Mark Moore <mark.moore@revature.com>
2020-09-08 14:36:33 -0500

    adds the first Delegates demo

---
+++ Demos/Delegates/SingleAndMulticastDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate double PerformCalculation(double x, double y);
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            PerformCalculation pc = Multiply;
+
+            System.Console.WriteLine(pc(5.0, 6.0));
+
+            pc += divide;
+            pc += Subtract;
+            // System.Console.WriteLine(pc(5.0, 6.0));
+
+            foreach (var item in pc.GetInvocationList())
+            {
+                System.Console.WriteLine(item.DynamicInvoke(5.0, 6.0));
+            }
+        }
+
+        // method 1
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        //method 2
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        //method 3
+        public static double divide(double x, double y)
+        {
+            return x / y;
+        }
+
+    }
+}
